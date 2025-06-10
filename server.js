88e4dc1b1f1036de548be7d71a6416c07ecd31a1
Carlos Cares <carlos.cares@ceisufro.cl>
2025-06-10 15:35:52 -0400

    prueba de variables CI/CD

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-const dotenv = require('dotenv');
+//const dotenv = require('dotenv');
 const fs = require('fs');
 const cors = require('cors');
-dotenv.config();
+//dotenv.config();
 
 const app = express();
 const PORT = 5000;
